de5478ce352752075f5bea23ad0f3a79a87adefd
seraph <quietsolitvde@gmail.com>
2026-04-13 20:35:52 +0700

    Finish exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    if (n<0) return "ERROR";
+    let result = "";
+    for(i=0 ; i<n ; i++) {
+        result += string;
+    }
+    return result;
 };
+
+repeatString('', 10)
 
 // Do not edit below this line
 module.exports = repeatString;
