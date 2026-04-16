fa59fb2add23a03196acd992c4da2cc972665d91
seraph <quietsolitvde@gmail.com>
2026-04-16 20:46:01 +0700

    Finish exercise 7 and 8

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = string.split("")
+    return stringArray.reverse().join("");
 };
+
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.');
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removals) {
+    return array.filter( item => !removals.includes(item) )
 };
+
+removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
+removeFromArray(["hey", 2, 3, "ho"], "hey", 3)
+removeFromArray([1, 2, 3], "1", 3)
 
 // Do not edit below this line
 module.exports = removeFromArray;
