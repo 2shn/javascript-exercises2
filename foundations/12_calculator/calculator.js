6e4dbf446dc37d18ef3715ac86a1b0ae5a415cb1
seraph <youremail@example.com>
2026-05-03 21:51:08 +0700

    Finish calculator hahahaha

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((acc, item) => acc + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, item) => acc * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (!number || number === 1) return 1;
+    let currentNumber = number;
+  for ( let i = 1 ; i < number ; i++ ) {
+    currentNumber *= i;
+  }
+  return currentNumber;
 };
+
+factorial(5);
 
 // Do not edit below this line
 module.exports = {
